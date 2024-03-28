d94ef096a90d1918bcdbf798d44681a4bfc9d367
Gabriel Ittner <gabriel.ittner@gmail.com>
2024-03-28 19:09:28 +0100

    Make record task cacheable

--- paparazzi/src/main/java/app/cash/paparazzi/Paparazzi.kt
+++ paparazzi/src/main/java/app/cash/paparazzi/Paparazzi.kt
@@ -157,7 +157,7 @@ public class Paparazzi @JvmOverloads constructor(
       if (isVerifying) {
         SnapshotVerifier(maxPercentDifference)
       } else {
-        HtmlReportWriter()
+        SnapshotWriter()
       }
   }
 }
